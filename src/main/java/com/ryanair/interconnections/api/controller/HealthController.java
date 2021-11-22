9fd75a55dffc1f32ac5fed76b80e7fe2a99abb35
Nicolas Camilli <ncamilli@redhat.com>
2021-11-22 16:31:51 +0100

    Added HealthController

---
+++ src/main/java/com/ryanair/interconnections/api/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.ryanair.interconnections.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class HealthController {
+    
+    @GetMapping
+    public HttpStatus health() {return HttpStatus.OK;}
+}
