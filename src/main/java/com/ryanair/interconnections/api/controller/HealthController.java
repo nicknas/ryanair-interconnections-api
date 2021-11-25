675d617ff62c0a699367d91b3875140a21627a2d
Nicolas Camilli <ncamilli@redhat.com>
2021-11-25 10:05:40 +0100

    Updated health check with a no content response

--- src/main/java/com/ryanair/interconnections/api/controller/HealthController.java
+++ src/main/java/com/ryanair/interconnections/api/controller/HealthController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
     
     @GetMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void health() {}
 }
